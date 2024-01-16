17df83f2e28ba31ada9e4d17dd874a56c6ef4a6b
sean <emotionfxxk@gmail.com>
2024-01-17 01:16:42 +0800

    clean up the code

--- app/src/main/java/com/argon/blue/matrixlivewallpaper/MatrixLiveWallpaperService.kt
+++ app/src/main/java/com/argon/blue/matrixlivewallpaper/MatrixLiveWallpaperService.kt
@@ -4,14 +4,8 @@ import android.graphics.Bitmap
 import android.graphics.BlurMaskFilter
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.ColorMatrix
-import android.graphics.ColorMatrixColorFilter
 import android.graphics.Paint
-import android.graphics.LinearGradient
-import android.graphics.Matrix
-import android.graphics.Path
 import android.graphics.PixelFormat
-import android.graphics.Shader
 import android.graphics.Typeface
 import android.os.Handler
 import android.os.HandlerThread
@@ -23,21 +17,18 @@ import kotlin.random.Random
 
 class MatrixLiveWallpaperService  : WallpaperService(){
     override fun onCreateEngine(): WallpaperService.Engine {
-        return MyWallpaperEngine()
+        return MatrixWallpaperEngine()
     }
 
-    private inner class MyWallpaperEngine : WallpaperService.Engine() {
+    private inner class MatrixWallpaperEngine : WallpaperService.Engine() {
         private var matrixCharset : String = "゠アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレワヰヱヲンヺ・ーヽヿ0123456789"
         //private var matrixCharset : String = "゠ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ0123456789"
         //private var matrixCharset : String = "゠абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789"
         //private var matrixCharset : String = "゠กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรฤลฦวศษสหฬอฮะาำเแโใไๅ็่้๊๋์0123456789"
         //private var matrixCharset : String = "鼠牛虎兔龍蛇馬羊猴雞狗豬水火木金土仁義禮智信命愛和誌氣力道喜福夢強心忠勇德禪緣壽榮幸美花富錢囍豐貴神恨死活鬼高低矮靜醜輸勝贏飛悲歡吻怒笑性煩誠忍弟姐妹妻夫父母友春夏秋冬"
-        private var rainbowSpeed : Float = 1.75f
-        private var rainbowOn : Boolean = true
-        private var rainbowLightness: Int = 45
-        private var rainbowSaturation: Int = 100
+
         private var speedInMillion:Long = 60
-        private var matrixTextSize:Float =  12f // size in dip
+        private var matrixTextSize:Float =  10f // size in dip
         private var matrixColumns:Int = 0
         private var chWidth:Float = 0f
         private var dropsEndIndex = mutableListOf<Int>()
@@ -48,8 +39,6 @@ class MatrixLiveWallpaperService  : WallpaperService(){
         private lateinit var matrixPaint:Paint
         private var drawingHandler: Handler? = null
         private var runnable: Runnable? = null
-        private var gradient:LinearGradient ? = null
-        private val matrix = Matrix()
 
         private var bufferBitmap: Bitmap? = null
         private var bufferCanvas: Canvas? = null
@@ -95,7 +84,7 @@ class MatrixLiveWallpaperService  : WallpaperService(){
                 isAntiAlias = true
                 textAlign = Paint.Align.CENTER
                 typeface = Typeface.create(Typeface.MONOSPACE, Typeface.NORMAL) // 设置字体样式
-                maskFilter = BlurMaskFilter(2f, BlurMaskFilter.Blur.SOLID)
+                maskFilter = BlurMaskFilter(4f, BlurMaskFilter.Blur.SOLID)
             }
             matrixPaint.setShadowLayer(40f, 2f, 2f, Color.GREEN)
 
@@ -106,27 +95,13 @@ class MatrixLiveWallpaperService  : WallpaperService(){
             lenOfVisibleString = (lenOfVerticalString / 2f).toInt()
             Log.d("GAGA", "displayMetrics H:${resources.displayMetrics.heightPixels}")
 
-            /*
-            gradient = LinearGradient(
-                resources.displayMetrics.widthPixels / 2f,
-                0f,
-                resources.displayMetrics.widthPixels / 2f,
-                lenOfVisibleString * matrixPaint.textSize,
-                intArrayOf(Color.TRANSPARENT,
-                    Color.parseColor("#7F00C800").toInt(),
-                    Color.parseColor("#FF00FF46").toInt(),
-                    Color.parseColor("#FF00FF00").toInt(),
-                    Color.WHITE),
-                null,
-                Shader.TileMode.CLAMP
-            )
-            matrixPaint.shader = gradient*/
+
             for(i in 0 until matrixColumns) {
                 dropsEndIndex.add(Random.nextInt(lenOfVerticalString))
                 stringMatrix.add(generateRandomString(matrixCharset, lenOfVerticalString))
             }
 
-            var drawingThread = HandlerThread("MatrixDrawingThread")
+            val drawingThread = HandlerThread("MatrixDrawingThread")
             drawingThread.start()
             drawingHandler= Handler(drawingThread.looper)
             runnable = object : Runnable {
@@ -169,43 +144,18 @@ class MatrixLiveWallpaperService  : WallpaperService(){
             var canvas:Canvas? = null
 
             try {
-                //canvas = holder.lockCanvas()
                 bufferCanvas!!.drawColor(0x10000000)
                 for (col in 0 until matrixColumns) {
                     val xPos = chWidth / 2f + chWidth * col
                     var yPos = matrixPaint.textSize / 2f
-                    /*
-                    gradient?.setLocalMatrix(matrix.apply {
-                        setTranslate(0f, (dropsEndIndex[col] - lenOfVisibleString) * matrixPaint.textSize)
-                    })*/
-                    var strColumn = stringMatrix.get(col)
-                    var startIndex = 0
-                    var endIndex = dropsEndIndex[col]
-                    if (endIndex < lenOfVisibleString ) {
-                        startIndex = 0
-                    } else if (endIndex >= lenOfVerticalString) {
-                        startIndex = endIndex - lenOfVisibleString
-                        endIndex = lenOfVerticalString - 1
-                    } else {
-                        startIndex = endIndex - lenOfVisibleString
-                    }
+                    val strColumn = stringMatrix[col]
+                    val endIndex = dropsEndIndex[col]
+                    bufferCanvas!!.drawText(strColumn.get(endIndex).toString(), xPos, yPos + endIndex * matrixPaint.textSize, matrixPaint)
 
-                    if(endIndex > startIndex) {
-                        var visibleStr = strColumn.substring(startIndex, endIndex)
-                        /*
-                        for (i in 0 until visibleStr.length) {
-                            var ch = visibleStr[i].toString()
-                            bufferCanvas!!.drawText(ch.toString(), xPos, yPos + startIndex * matrixPaint.textSize, matrixPaint)
-                            yPos += matrixPaint.textSize
-                        }*/
-                        var ch = visibleStr[visibleStr.length - 1].toString()
-                        bufferCanvas!!.drawText(ch.toString(), xPos, yPos + endIndex * matrixPaint.textSize, matrixPaint)
-                        //yPos += matrixPaint.textSize
-                    }
                     dropsEndIndex[col] = dropsEndIndex[col] + 1
-                    if(dropsEndIndex[col] > lenOfVerticalString + lenOfVisibleString - 1) {
-                        dropsEndIndex[col] = 0;
-                        stringMatrix.set(col, generateRandomString(matrixCharset, lenOfVerticalString))
+                    if(dropsEndIndex[col] > lenOfVerticalString -1) {
+                        dropsEndIndex[col] = 0
+                        stringMatrix[col] = generateRandomString(matrixCharset, lenOfVerticalString)
                     }
                 }
                 canvas = holder.lockCanvas()
